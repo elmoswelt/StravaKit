43df105155bf1c8ed5228f09237daecbf55e5e5d
Elmar Tampe <elmo@mountaincoders.ch>
2014-12-03 17:49:30 +0100

    Project setup - StravaKit Framework targets

---
+++ StravaKit/StravaKit/StravaKit.h
@@ -0,0 +1,19 @@
+//
+//  StravaKitiOS.h
+//  StravaKitiOS
+//
+//  Created by Elmar Tampe on 03/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StravaKitiOS.
+FOUNDATION_EXPORT double StravaKitiOSVersionNumber;
+
+//! Project version string for StravaKitiOS.
+FOUNDATION_EXPORT const unsigned char StravaKitiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StravaKitiOS/PublicHeader.h>
+
+
