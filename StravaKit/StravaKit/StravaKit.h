1106866fe3d17c735fec15b69247539b6f23956d
Elmar Tampe <elmo@mountaincoders.ch>
2014-12-04 17:20:27 +0100

    Repair

---
+++ StravaKit/StravaKit/StravaKit.h
@@ -0,0 +1,19 @@
+//
+//  StravaKitOSX.h
+//  StravaKitOSX
+//
+//  Created by Elmar Tampe on 04/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StravaKitOSX.
+FOUNDATION_EXPORT double StravaKitOSXVersionNumber;
+
+//! Project version string for StravaKitOSX.
+FOUNDATION_EXPORT const unsigned char StravaKitOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StravaKitOSX/PublicHeader.h>
+
+
