b7c85e818713b7b7fcceb968f4d63f95412a2b61
Elmar Tampe <elmo@mountaincoders.ch>
2014-12-05 17:32:28 +0100

    StravaKitClient can be used as a singleton now.

--- StravaKit/StravaKit/Source/API/StravaKitClient.swift
+++ StravaKit/StravaKit/Source/API/StravaKitClient.swift
@@ -9,7 +9,33 @@
 import Foundation
 
 @objc (MCStravaKitClient) public class StravaKitClient : NSObject {
+
     
+    // ------------------------------------------------------------------------------------------
+    //MARK: - Singleton
+    // ------------------------------------------------------------------------------------------
+    
+    class var shared : StravaKitClient {
+        
+        struct Static {
+            static let instance : StravaKitClient = StravaKitClient()
+        }
+        
+        return Static.instance
+    }
+    
+    // For Objective-C convenience - Use [StravaKitClient sharedInstance] as usual. use StravaClient.shared 
+    // when using the framework in swift.
+    
+    class public func sharedInstance() -> StravaKitClient {
+    
+        return StravaKitClient.shared
+    }
+    
+    
+    // ------------------------------------------------------------------------------------------
+    //MARK: - Test
+    // ------------------------------------------------------------------------------------------
     public func helloWorld() {
     
         println("Strava rockz!")
