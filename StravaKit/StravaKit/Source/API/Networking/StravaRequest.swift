05cc91089f0ae44e6c589e7cafd8ea1b516edef5
Elmar Tampe <elmo@mountaincoders.ch>
2014-12-10 16:37:44 +0100

    Added base class for requests
    Refactoring

---
+++ StravaKit/StravaKit/Source/API/Networking/StravaRequest.swift
@@ -0,0 +1,13 @@
+//
+//  StravaRequest.swift
+//  StravaKit
+//
+//  Created by Elmar Tampe on 10/12/14.
+//
+//
+
+import Foundation
+
+@objc  (MCStravaRequest) public class StravaRequest : NSObject {
+
+}
